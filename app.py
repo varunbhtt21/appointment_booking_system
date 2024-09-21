0ccacb9d4e3e3c4ace3923fe43947f26a487fa40
Varun Bhatt <42655062+varunbhtt21@users.noreply.github.com>
2024-09-21 15:47:18 +0530

    Fix DATABASE_URL prefix for SQLAlchemy compatibility

--- app.py
+++ app.py
@@ -7,16 +7,31 @@ from flask import Flask
 from flask_restful import Api
 from database import db  # Import db from database.py
 from dotenv import load_dotenv
+from models import Appointment  
+from flask_migrate import Migrate
+from urllib.parse import urlparse, urlunparse
+
 
 load_dotenv()  # Load variables from .env file
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///appointments.db')
+# Retrieve the DATABASE_URL from environment variables
+db_url = os.environ.get('DATABASE_URL', 'sqlite:///appointments.db')
+
+# Check if the URL starts with 'postgres://' and replace it with 'postgresql://'
+if db_url.startswith('postgres://'):
+    db_url = db_url.replace('postgres://', 'postgresql://', 1)
+
+# Configure the database using the corrected DATABASE_URL
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize db with app
 db.init_app(app)
+
+# After initializing db
+migrate = Migrate(app, db)
 
 # Initialize Flask-RESTful API
 api = Api(app)
